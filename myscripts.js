df53e52dbbb77ff17db8872b736d1e385aa57f24
Kirill <shishkokirill.fudosin@gmail.com>
2022-08-18 16:53:01 +0300

    Buttons disappear when score is up to 5

--- myscripts.js
+++ myscripts.js
@@ -23,8 +23,8 @@ rock.addEventListener('click', () => {
     console.log('computer choice: ' + computerSelection);
 
     let changeScore = document.querySelector('.Score');
-        let playerScore = document.querySelector('.playerScore');
-        let computerScore = document.querySelector('.computerScore');
+    let playerScore = document.querySelector('.playerScore');
+    let computerScore = document.querySelector('.computerScore');
 
     if(computerSelection === "ROCK") {
         user++;
@@ -54,25 +54,136 @@ rock.addEventListener('click', () => {
         oldUserScore.textContent = user;
         changeScore.replaceChild(oldUserScore, playerScore);
     }
+
+    checkScore();
+});
+rock.addEventListener('click', () => {
+    if(user === 5 || computer === 5) {
+        let container = document.querySelector('.containerForButoons');
+        let firstButton = document.querySelector('.Rock'); 
+        let secondButton = document.querySelector('.Paper');
+        let thirdButton = document.querySelector('.Scissors');
+        container.removeChild(firstButton);  
+        container.removeChild(secondButton);  
+        container.removeChild(thirdButton);  
+    }
 });
 
+const paper = document.querySelector('.Paper');
+paper.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    console.log('computer choice: ' + computerSelection);
 
+    let changeScore = document.querySelector('.Score');
+    let playerScore = document.querySelector('.playerScore');
+    let computerScore = document.querySelector('.computerScore');
 
+    if(computerSelection === "PAPER") {
+        user++;
+        computer++;
+        
+        let oldUserScore = document.createElement('div');
+        oldUserScore.classList.add('playerScore');
+        oldUserScore.textContent = user;
+        changeScore.replaceChild(oldUserScore, playerScore);
 
+        let oldCompScore = document.createElement('div');
+        oldCompScore.classList.add('computerScore');
+        oldCompScore.textContent = computer;
+        changeScore.replaceChild(oldCompScore, computerScore);
+    }
+    else if(computerSelection === "SCISSORS") {
+        computer++;
+        let oldCompScore = document.createElement('div');
+        oldCompScore.classList.add('computerScore');
+        oldCompScore.textContent = computer;
+        changeScore.replaceChild(oldCompScore, computerScore);
+    }
+    else {
+        user++;
+        let oldUserScore = document.createElement('div');
+        oldUserScore.classList.add('playerScore');
+        oldUserScore.textContent = user;
+        changeScore.replaceChild(oldUserScore, playerScore);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-/*const paper = document.querySelector('.Paper');
-paper.addEventListener('click', () => console.log('paper'));
+    checkScore();
+});
+paper.addEventListener('click', () => {
+    if(user === 5 || computer === 5) {
+        let container = document.querySelector('.containerForButoons');
+        let firstButton = document.querySelector('.Rock'); 
+        let secondButton = document.querySelector('.Paper');
+        let thirdButton = document.querySelector('.Scissors');
+        container.removeChild(firstButton);  
+        container.removeChild(secondButton);  
+        container.removeChild(thirdButton);  
+    }
+});
 
 const scissors = document.querySelector('.Scissors');
-scissors.addEventListener('click', () => console.log('scissors'));*/
+scissors.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    console.log('computer choice: ' + computerSelection);
+
+    let changeScore = document.querySelector('.Score');
+    let playerScore = document.querySelector('.playerScore');
+    let computerScore = document.querySelector('.computerScore');
+
+    if(computerSelection === "SCISSORS") {
+        user++;
+        computer++;
+        
+        let oldUserScore = document.createElement('div');
+        oldUserScore.classList.add('playerScore');
+        oldUserScore.textContent = user;
+        changeScore.replaceChild(oldUserScore, playerScore);
+
+        let oldCompScore = document.createElement('div');
+        oldCompScore.classList.add('computerScore');
+        oldCompScore.textContent = computer;
+        changeScore.replaceChild(oldCompScore, computerScore);
+    }
+    else if(computerSelection === "ROCK") {
+        computer++;
+        let oldCompScore = document.createElement('div');
+        oldCompScore.classList.add('computerScore');
+        oldCompScore.textContent = computer;
+        changeScore.replaceChild(oldCompScore, computerScore);
+    }
+    else {
+        user++;
+        let oldUserScore = document.createElement('div');
+        oldUserScore.classList.add('playerScore');
+        oldUserScore.textContent = user;
+        changeScore.replaceChild(oldUserScore, playerScore);
+    }
+
+    checkScore();
+});
+scissors.addEventListener('click', () => {
+    if(user === 5 || computer === 5) {
+        let container = document.querySelector('.containerForButoons');
+        let firstButton = document.querySelector('.Rock'); 
+        let secondButton = document.querySelector('.Paper');
+        let thirdButton = document.querySelector('.Scissors');
+        container.removeChild(firstButton);  
+        container.removeChild(secondButton);  
+        container.removeChild(thirdButton);  
+    }
+});
+
+function checkScore() {
+    if(user === 5 && computer === 5) {
+        console.log('draw');
+        return user;
+    }
+    else if(user === 5 && computer !== 5) {
+        console.log('You are a winner!');
+        return user;
+    }
+    else if(user !== 5 && computer === 5) {
+        console.log('You are a loser!');
+        return computer;
+    }
+}
